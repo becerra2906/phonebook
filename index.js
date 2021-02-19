27d88f35694e9ea8665601a91f5bbe4123fb3adf
Alejandro <alejobecerra1995@gmail.com>
2021-02-18 23:07:52 -0500

    added initial logic for phonebook server and endpoints for getting contacts saved, accessing a single contact, and deleting a contact

---
+++ index.js
@@ -0,0 +1,94 @@
+const { json } = require('express');
+const express = require('express');
+const app = express();
+const port =  3001;
+const contacts = [
+    {
+        firstName: "Alejandro",
+        surname: "Becerra",
+        phone: 320998820,
+        github: "becerra2906",
+        work: "Instaleap",
+        role: "Product Manager",
+        id: 1
+
+    }
+];
+const idGenerator = contacts.length + 1;
+
+
+
+app.use(json());
+
+app.get('/api',(req,res)=>{
+    res.send("<p>This api allows you to access and add contacts to a phone directory</p>");
+})
+
+app.get('/info',(req,res)=>{
+    let date = new   Date
+    res.send(`This phonebook has info for ${contacts.length} contacts </br>${date}`);
+})
+
+///const newContact = function  newcontacts (firstName, surname, number, address, email){
+///    firstName = String(this.name),
+///    surname = String(this.surname),
+///    number = Number(this.number),
+///    address = String(this.address),
+///    email = String(this.email),
+///    id = Number(contacts.length + 1)
+/// }
+
+app.post('/api/contacts',(req,res)=>{
+    const contactInfo = req.body
+    contactInfo.id = idGenerator;
+    contacts.push(contactInfo);
+    if (contactInfo){
+        console.log(contacts);
+        return res.send(`Contact created successfully`);
+    }
+    else {
+        res.status(400);
+    }
+
+});
+
+app.get('/api/contacts',(req,res)=>{
+    if (contacts.length > 0){
+        res.send(contacts);
+    }
+    else{
+        res.status(204);
+        res.send('No contacts available')
+    }
+})
+
+app.get('/api/contacts/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    const contact = (contacts.find(contactSearched => contactSearched.id === id));
+    if (contact){
+        res.send(contact);
+    }
+    else{
+        res.status(204);
+        res.send('No contact available with that ID');
+        res.console.log('No contact available with that ID');
+    }
+})
+
+app.delete('/api/contacts/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    const remainingContacts = (contacts.filter(contactDeleted => contactDeleted.id !== id));
+    
+    if (remainingContacts){
+        res.send(remainingContacts);
+    }
+    else{
+        res.status(204);
+        res.send('No contact available with that ID');
+        res.console.log('No contact available with that ID');
+    }
+})
+
+app.listen(port,(req,res) => {
+    console.log (`Server is running on ${port}`)
+} )
